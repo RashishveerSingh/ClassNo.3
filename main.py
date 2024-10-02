ef841d05acddced2aefd27ad620a68cb9ceecf52
RashishveerSingh <ashstormsaber@gmail.com>
2024-10-02 21:13:50 +0800

    initial

---
+++ main.py
@@ -0,0 +1,29 @@
+#activity 1 and 2
+
+i = 14
+f = 1.4
+b = True
+s = "programming"
+
+print(type(i),type(f),type(b),type(s))
+
+#example of type casting
+
+g = str(f)
+print(type(g))
+
+#example of slicing
+l = "programming"
+print(l[::])
+print(l[0::1])
+print(l[0:8:])
+
+#example of revesing a string
+
+print(l[::-1])
+
+#example of concatenation
+
+w = "244"
+Q = "144"
+print(w +" "+Q)
